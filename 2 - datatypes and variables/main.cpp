7a2ffbbddf74325508a607dbae9a63d54cbd7d3f
jaydeep-thakor <imjaydeepcreation@gmail.com>
2026-01-07 08:29:30 +0530

    new code added

---
+++ 2 - datatypes and variables/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int a = 7;
+    cout << a << endl;
+
+    char b = 'j';
+    cout << b << endl;
+
+    bool c = true;
+    cout << c << endl;
+
+    float d = 1.2;
+    cout << d << endl;
+
+    double e = 1.23;
+    cout << e << endl;
+
+    int sizeA = sizeof(a);
+    cout << "size of A is = " << sizeA << endl;
+
+    int sizeE = sizeof(e);
+    cout << "size of E is = " << sizeE << endl;
+
+}
